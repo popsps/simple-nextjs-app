99b1fd555b64fe2b1da233d77f25169e503c7142
popsps <59066663+popsps@users.noreply.github.com>
2020-04-11 21:59:12 -0400

    first serverless function

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,63 @@
 import Layout from "../components/layout";
 
-const Home = () => (
-    <Layout>
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const getRequest = async (endpoint) => {
+  const res = await fetch(`/api/${endpoint}`)
+  const data = await res.json()
+}
+const Home = () => {
+  const user = getRequest('user')
+  console.log(user)
+  return (
+      <Layout>
+        <main>
+          <h1 className="title">
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <p className="description">
+            Get started by editing <code>pages/index.js</code>
+          </p>
+          <p>Hello {user.name}</p>
+          <div className="grid">
+            <a href="https://nextjs.org/docs" className="card">
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <a href="https://nextjs.org/learn" className="card">
+              <h3>Learn &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a
+                href="https://github.com/zeit/next.js/tree/master/examples"
+                className="card"
+            >
+              <h3>Examples &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
 
+            <a
+                href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className="card"
+            >
+              <h3>Deploy &rarr;</h3>
+              <p>
+                Instantly deploy your Next.js site to a public URL with ZEIT Now.
+              </p>
+            </a>
+          </div>
+        </main>
+        <footer>
           <a
-              href="https://github.com/zeit/next.js/tree/master/examples"
-              className="card"
+              href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            Powered by <img src="/zeit.svg" alt="ZEIT Logo"/>
           </a>
-
-          <a
-              href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
-        </div>
-      </main>
-      <footer>
-        <a
-            href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo"/>
-        </a>
-      </footer>
-      <style jsx>{`
+        </footer>
+        <style jsx>{`
       .container {
         min-height: 100vh;
         padding: 0 0.5rem;
@@ -176,7 +183,7 @@ const Home = () => (
         }
       }
     `}</style>
-      <style jsx global>{`
+        <style jsx global>{`
       html,
       body {
         padding: 0;
@@ -189,7 +196,8 @@ const Home = () => (
         box-sizing: border-box;
       }
     `}</style>
-    </Layout>
-)
+      </Layout>
+  )
+}
 
 export default Home
